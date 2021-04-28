09274a5dfa53ea357eaddc6db8837dadc594ef3d
lei <lei.wang0418@gmail.com>
2021-04-29 00:44:22 +0800

    button test 添加 describe

--- packages/ra-material-ui/src/components/button/Button.test.tsx
+++ packages/ra-material-ui/src/components/button/Button.test.tsx
@@ -4,14 +4,16 @@ import '@testing-library/jest-dom/extend-expect';
 
 import { Basic, WithArgs } from './Button.stories';
 
-it('renders the button in the Basic state', () => {
-	render(<Basic />);
+describe('Button test', () => {
+	it('renders the button in the Basic state', () => {
+		render(<Basic />);
 
-	expect(screen.getByRole('button').textContent).toBe('Click me');
-});
+		expect(screen.getByRole('button').textContent).toBe('Click me');
+	});
 
-it('renders the button in the WithArgs state', () => {
-	render(<WithArgs label={"no args"} />);
-	
-	expect(screen.getByRole('button').textContent).toBe('no args');
+	it('renders the button in the WithArgs state', () => {
+		render(<WithArgs label={'no args'} />);
+
+		expect(screen.getByRole('button').textContent).toBe('no args');
+	});
 });
