414dd885fa500f857d6e9c6d76dc46807cc75586
lei <lei.wang0418@gmail.com>
2021-05-08 23:48:40 +0800

    test.utils修改typescript验证

--- packages/test-sample/src/util/test-utils.tsx
+++ packages/test-sample/src/util/test-utils.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { render as rtlRender } from '@testing-library/react';
+import { render as rtlRender, RenderOptions } from '@testing-library/react';
 import { IntlProvider } from 'react-intl';
 import messages from '../locale';
 
 const userLocale = "zh-CN";
 
 const render = (
-	ui: React.ReactElement<any, string | React.JSXElementConstructor<any>>,
-	{ ...renderOptions } = {}
+	ui: React.ReactElement,
+	renderOptions?: Omit<RenderOptions, 'queries'>,
 ) => {
 	
 	const Wrapper: React.FC = ({ children }) => (
