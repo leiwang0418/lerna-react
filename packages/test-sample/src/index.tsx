c8b23094a03a31a27e45ee7a41369cb64295c0c6
leiwang0418 <lei.wang0418@gmail.com>
2021-05-14 15:19:42 +0800

    调整test-utils使用zh国际化资源

--- packages/test-sample/src/index.tsx
+++ packages/test-sample/src/index.tsx
@@ -18,7 +18,7 @@ const bootstrapApp = async (locale: string, mainDiv: HTMLElement | null) => {
 	);
 }
 
-bootstrapApp('en', document.getElementById('root'));
+bootstrapApp('zh', document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- packages/test-sample/src/util/test-utils.tsx
+++ packages/test-sample/src/util/test-utils.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { render as rtlRender, RenderOptions } from '@testing-library/react';
 import { IntlProvider } from 'react-intl';
-import messages from '../compiled-lang/zh.json';
+import zh from '../compiled-lang/zh.json';
 
 const render = (
 	ui: React.ReactElement,
-	{locale='zh', messages={messages}, ...renderOptions} = {}
+	{locale='zh', ...renderOptions} = {}
 ) => {
 	const Wrapper: React.FC = ({ children }) => (
 		<IntlProvider locale={locale} messages={zh}>{children}</IntlProvider>
