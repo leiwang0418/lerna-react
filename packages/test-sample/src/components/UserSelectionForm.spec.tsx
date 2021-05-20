5ec944c90ebd4890c0a8e5aa204eb8f5c602afda
leiwang0418 <lei.wang0418@gmail.com>
2021-05-20 16:38:15 +0800

    完全重构已有代码单元测试

--- packages/test-sample/src/components/UserSelectionForm.spec.tsx
+++ packages/test-sample/src/components/UserSelectionForm.spec.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 import { render, screen, fireEvent } from '../utils/test-utils';
 import UserSelectionForm from './UserSelectionForm';
 
-import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
-
 describe('snapshot test', () => {
 	it('renders the basic UserSelectionForm', () => {
 		const { container } = render(<UserSelectionForm />);
 
 		expect(container).toMatchSnapshot();
 
-		expect(screen.getByPlaceholderText(/请输入GitHub用户名/i)).toBeInTheDocument();
+		expect(
+			screen.getByPlaceholderText(/请输入GitHub用户名/i)
+		).toBeInTheDocument();
 		expect(screen.getByDisplayValue(/leiwang0418/i)).toBeInTheDocument();
 		expect(screen.getByRole('button')).toHaveTextContent('查看仓库列表');
 	});
